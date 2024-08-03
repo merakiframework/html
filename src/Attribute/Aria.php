0f5b0b0d2f98ed884e4ee9fa0164a41266f99aec
Nathan <nbish11@hotmail.com>
2024-08-03 08:43:17 +0000

    Fix copy-pasting of 'data' attribute code

--- src/Attribute/Aria.php
+++ src/Attribute/Aria.php
@@ -5,17 +5,17 @@ namespace Meraki\Html\Attribute;
 
 use Meraki\Html\Attribute;
 
-final class Data extends Attribute
+final class Aria extends Attribute
 {
 	/**
 	 * Constructor.
 	 *
-	 * @param string $name The name of the data attribute, excluding the "data-" prefix.
-	 * @param string $value The value of the data attribute.
+	 * @param string $name The name of the aria attribute, excluding the "aria-" prefix.
+	 * @param string $value The value of the aria attribute.
 	 */
 	public function __construct(string $name, string $value)
 	{
-		$this->setName('data-' . $name);
+		$this->setName('aria-' . $name);
 		$this->setValue($value);
 	}
 }
